3d1dfc10664657b059e796d42f167cbc4361789f
hayeon7898 <hayeon22@korea.ac.kr>
2026-09-04 00:37:55 +0900

    fix: ruff format 관련 에러 수정 (#29)

--- test.py
+++
@@ -1,5 +0,0 @@
-from app.detect.ner import get_ner_engine, GLINER_LABELS
-
-engine = get_ner_engine()
-engine.preload()
-print(engine._model.predict_entities("신용등급도 확인해주세요", GLINER_LABELS, threshold=0.0))
